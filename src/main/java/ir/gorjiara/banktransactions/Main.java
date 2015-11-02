8cb79d1173d5a263d0a5206fc3617bfeeec707ed
Hamed Ara <Hamed Ara@192.168.1.62>
2015-11-02 17:54:36 +0330

    first commit

---
+++ src/main/java/ir/gorjiara/banktransactions/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ir.gorjiara.banktransactions;
+
+import org.json.simple.parser.JSONParser;
+
+/**
+ *
+ * @author Hamed Ara
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hi");
+        JSONParser parser = new JSONParser();
+        
+    }
+    
+}
