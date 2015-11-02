89aac7212134f5f03a20d41ed94372fe0a4e5493
Hamed Ara <Hamed Ara@192.168.1.62>
2015-11-02 18:31:09 +0330

    first commit

--- src/main/java/net/gorjiara/banktransactions/Main.java
+++ src/main/java/net/gorjiara/banktransactions/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ir.gorjiara.banktransactions;
+package net.gorjiara.banktransactions;
 
 import org.json.simple.parser.JSONParser;
 
